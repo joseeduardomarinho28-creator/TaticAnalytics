2fe1cccd5eee4db927dc29bc36ac83377bca2110
Eduardo Marinho <joseeduardomarinho28@gmail.com>
2026-09-11 08:46:33 -0300

    feat: inspect and log frame entities in Main application

--- java_core/src/main/java/com/taticanalytics/Main.java
+++ java_core/src/main/java/com/taticanalytics/Main.java
@@ -4,6 +4,9 @@ import com.taticanalytics.service.TrackingDataLoader;
 import java.util.List;
 import com.taticanalytics.model.FrameData;
 import com.taticanalytics.model.Entity;
+import com.taticanalytics.model.Player;
+import com.taticanalytics.model.Referee;
+import com.taticanalytics.model.Ball;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,6 +15,27 @@ public class Main {
         List<FrameData> frames = loader.loadData("src/main/resources/tracking_sample.json");
 
         System.out.println("Frames : " + frames.size());
+
+        System.out.println("--------------------------------------------------");
+
+        for (FrameData frame : frames) {
+            System.out.println("Frame ID: " + frame.getFrameId() + " | Time: " + frame.getTimestamp() + "s");
+
+            for (Entity entity : frame.getEntities()) {
+
+                 if (entity instanceof Player player) {
+                System.out.println("[PLAYER] ID: " + player.getId() + " | Team: " + player.getTeamId() + " | Position: (" + player.getX() + ", " + player.getY() + ")");
+            }
+
+            if (entity instanceof Ball ball) {
+                System.out.println("[BALL] ID: " + ball.getId() + " | Position: (" + ball.getX() + ", " + ball.getY() + ")");
+            }
+
+            if (entity instanceof Referee referee) {
+                System.out.println("[REFEREE] ID: " + referee.getId() + " | Position: (" + referee.getX() + ", " + referee.getY() + ")");
+            }
+            }
+        }
 
     }
 }
