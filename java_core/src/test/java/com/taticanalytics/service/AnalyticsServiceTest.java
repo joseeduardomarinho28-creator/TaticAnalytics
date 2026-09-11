dc2abe4e4198914a14a155c00ba5adbeef19ca26
Eduardo Marinho <joseeduardomarinho28@gmail.com>
2026-09-11 15:45:11 -0300

    test: add unit test for non-existent player scenario in AnalyticsService

--- java_core/src/test/java/com/taticanalytics/service/AnalyticsServiceTest.java
+++ java_core/src/test/java/com/taticanalytics/service/AnalyticsServiceTest.java
@@ -30,7 +30,25 @@ public class AnalyticsServiceTest {
 
         assertEquals(5.0, totalDistance, 0.001);
 
+    }
 
+    @Test
+    public void shouldReturnZeroWhenPlayerNotFound() {
+        List<FrameData> frames = new ArrayList<>();
+
+        FrameData frame1 = new FrameData(1, 0.0);
+        frame1.addEntity(new Player(1, 0.0, 0.0, 10));
+        frames.add(frame1);
+
+        FrameData frame2 = new FrameData(2, 0.1);
+        frame2.addEntity(new Player(1, 3.0, 4.0, 10));
+        frames.add(frame2);
+
+        AnalyticsService service = new AnalyticsService();
+
+        double totalDistance = service.calculateTotalDistance(frames, 99);
+
+        assertEquals(0.0, totalDistance, 0.001);
 
     }
 }
